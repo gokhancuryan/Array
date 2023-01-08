2be9116423653ef0ce2d31f6b844f3371e15366c
gokhancuryan <119778787+gokhancuryan@users.noreply.github.com>
2023-01-08 17:39:22 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          //Sort
+            int[] sayiDizisi = {23,12,4,86,72,3,11,17};
+            foreach (var sayi in sayiDizisi)
+            {
+                Console.WriteLine(sayi);
+            }
+            Array.Sort(sayiDizisi);
+
+            foreach (var sayi in sayiDizisi)
+            {
+                Console.WriteLine(sayi);
+            }
+
+            //clear
+            Array.Clear(sayiDizisi,2,2);
+
+            foreach (var sayi in sayiDizisi)
+            {
+                Console.WriteLine(sayi);
+            }
+
+            //Reverse
+
+            Array.Reverse(sayiDizisi);
+            foreach (var sayi in sayiDizisi)
+            {
+                Console.WriteLine(sayi);
+            }
+
+            //IndexOf
+
+            
+            Console.WriteLine(Array.IndexOf(sayiDizisi,23));
+
+            //Resize
+            Array.Resize<int>(ref sayiDizisi,9);
+            Console.WriteLine(sayiDizisi.Length);
+        }
+    }
+}
